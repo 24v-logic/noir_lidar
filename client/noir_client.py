a028ff53642d80bfdf58cb4e1e0a2e57526efb86
josephbryant <josephprine@gmail.com>
2017-02-06 23:33:13 -0500

    cleaned up code, created driver module, implemented double capture

--- client/noir_client.py
+++ client/noir_client.py
@@ -5,15 +5,11 @@ import picamera.array
 import thread
 import time
 import threading
-from rplidar import RPLidar
-
+import noir_lidar
 from cStringIO import StringIO
-from lidar_get import run
-rpyc.core.protocol.DEFAULT_CONFIG['allow_pickle']=True
-output = []
-lidar_frames = []
-cam_frames = []
+
 import sys
+rpyc.core.protocol.DEFAULT_CONFIG['allow_pickle']=True
 '''
 STDERR = sys.stderr
 def excepthook(*args):
@@ -21,45 +17,7 @@ def excepthook(*args):
 	print >> STDERR, args
 sys.excepthook = excepthook
 '''
-def capture():
 
-	with picamera.PiCamera() as camera:
-		with picamera.array.PiRGBArray(camera) as output:
-			camera.resolution = (1280,700)
-			camera.capture(output,'rgb')
-			print('Captured %d%d image' % (output.array.shape[1], output.array.shape[0]))
-			f = StringIO()
-			np.savez_compressed(f, frame=output.array)
-			f.seek(0)
-			return(f.read())
-
-
-def capture_camera(n):
-	print("camera")
-	frames = []
-	with picamera.PiCamera() as camera:
-		with picamera.array.PiRGBArray(camera) as output:
-			camera.resolution = (640,480)
-			for i in range(n):
-				camera.capture(output, 'rgb')
-				frames.append(output.array)
-				output.truncate(0)
-				
-	print("captured %s frames" % (n))
-	f = StringIO()
-	np.savez_compressed(f,frame=frames)
-	f.seek(0)
-	print "camera capture done"
-	return f.read()
-			
-def capture_lidar(n):
-	print("lidar")
-	lidar_data = run(n)
-	f = StringIO()
-	np.savez_compressed(f, frame=lidar_data)
-	f.seek(0)
-	print "lidar capture done"
-	return f.read()
 
 def payloadize(npyarray):
 	npyarray = np.array(npyarray)
@@ -75,8 +33,9 @@ if __name__=='__main__':
 	c = rpyc.connect(hostname,port)
 	service = c.root.get_shared()
 	while True:
-		service.acquire_frames('camera_frames',capture_frames(10))
-		#service.acquire_frames('camera_frames', capture_camera(10))
-		#service.acquire_frames('lidar_frames', capture_lidar(10))
-		
+		lidar_frames, camera_frames = noir_lidar.double_capture(10)
+		lidar_payload = payloadize(lidar_frames)
+		camera_payload = payloadize(camera_frames)
+		service.acquire_frames('lidar_frames', lidar_payload)
+		service.acquire_frames('camera_frames', camera_payload)
 		break

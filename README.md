002cfcb17acfa787db51b6c5dbf4edb823382151
josephbryant <josephprine@gmail.com>
2017-02-10 19:33:46 -0500

    Added hi_speed_capture

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # noir_lidar
+This is a graduate research project being developed for the Florida Polytechnic University VTC Lab.
+
+The goal is to synthesize the Raspberry Pi 3, Pi NoIR Camera, and RPLidar in an easy to use platform for students to experiment with computer vision concepts.
+
+This is currently a python project but it has aspirations of being more.
--- client/noir_lidar.py
+++ client/noir_lidar.py
@@ -16,7 +16,7 @@ def lidar_capture(n):
 			data.append(np.array(scan))
 			scan_count += 1
 			if scan_count > n:
-				
+
 				lidar.stop()
 				lidar.disconnect()
 				return np.array(data)
@@ -39,7 +39,36 @@ def image_capture(n):
 				camera_frames.append(output.array)
 				output.truncate(0)
 	return np.array(camera_frames)
-	
+
+def hi_speed_capture(n,m):
+lidar = RPLidar(PORT_NAME)
+lidar_frames = []
+image_frames = []
+try:
+	print('Recording lidar scans and camera images. . .')
+	scan_count = 0
+	for scan in lidar.iter_scans():
+		with picamera.PiCamera() as camera:
+			with picamera.array.PiRGBArray(camera) as output:
+				for i in range(m):
+					camera.resolution=(64,48)
+					camera.capture(output,'rgb')
+					image_frames.append(output.array)
+					output.truncate(0)
+		lidar_frames.append(np.array(scan))
+		scan_count += 1
+		if scan_count > n:
+			lidar.stop()
+			lidar.disconnect()
+			return np.array(lidar_frames), np.array(image_frames)
+			break
+
+except KeyboardInterrupt:
+	print('Stopping')
+lidar.stop()
+lidar.disconnect()
+
+
 def double_capture(n):
 
 	lidar = RPLidar(PORT_NAME)
@@ -67,6 +96,3 @@ def double_capture(n):
 		print('Stopping')
 	lidar.stop()
 	lidar.disconnect()
-	
-
-

05d27fbaa38dac124805097db0b2422d6ccaa1b3
josephbryant <josephprine@gmail.com>
2017-02-10 19:44:31 -0500

    Debug

--- client/noir_lidar.py
+++ client/noir_lidar.py
@@ -41,36 +41,35 @@ def image_capture(n):
 	return np.array(camera_frames)
 
 def hi_speed_capture(n,m):
-lidar = RPLidar(PORT_NAME)
-lidar_frames = []
-image_frames = []
-try:
-	print('Recording lidar scans and camera images. . .')
-	scan_count = 0
-	for scan in lidar.iter_scans():
-		with picamera.PiCamera() as camera:
-			with picamera.array.PiRGBArray(camera) as output:
-				for i in range(m):
-					camera.resolution=(64,48)
-					camera.capture(output,'rgb')
-					image_frames.append(output.array)
-					output.truncate(0)
-		lidar_frames.append(np.array(scan))
-		scan_count += 1
-		if scan_count > n:
-			lidar.stop()
-			lidar.disconnect()
-			return np.array(lidar_frames), np.array(image_frames)
-			break
+	lidar = RPLidar(PORT_NAME)
+	lidar_frames = []
+	image_frames = []
+	try:
+		print('Recording lidar scans and camera images. . .')
+		scan_count = 0
+		for scan in lidar.iter_scans():
+			with picamera.PiCamera() as camera:
+				with picamera.array.PiRGBArray(camera) as output:
+					for i in range(m):
+						camera.resolution=(64,48)
+						camera.capture(output,'rgb')
+						image_frames.append(output.array)
+						output.truncate(0)
+			lidar_frames.append(np.array(scan))
+			scan_count += 1
+			if scan_count > n:
+				lidar.stop()
+				lidar.disconnect()
+				return np.array(lidar_frames), np.array(image_frames)
+				break
 
-except KeyboardInterrupt:
-	print('Stopping')
-lidar.stop()
-lidar.disconnect()
+	except KeyboardInterrupt:
+		print('Stopping')
+	lidar.stop()
+	lidar.disconnect()
 
 
 def double_capture(n):
-
 	lidar = RPLidar(PORT_NAME)
 	lidar_frames = []
 	image_frames = []
